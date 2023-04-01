5c634e794efff0a18b0aa46d890928b46b509870
Faiz <agustianto.d19@gmail.com>
2023-04-01 23:10:17 +0700

    tidy up Weather.js

--- components/Weather.js
+++ components/Weather.js
@@ -2,9 +2,6 @@ import React from "react";
 import Image from "next/image";
 
 const Weather = ({ data }) => {
-    console.log(data);
-    console.log(data.current.condition.icon);
-    console.log(data.current.temp_c.toFixed(0))
   return (
     <div className="relative flex flex-col justify-between max-w-[500px] w-full h-[90vh] m-auto p-4 text-white">
       <div className="relative flex justify-between pt-12">
@@ -14,7 +11,7 @@ const Weather = ({ data }) => {
         </div>
         <p className="text-9xl">{data.current.temp_c.toFixed(0)}&#176;</p>
       </div>
-      <div className="bg-black/50 relative p-8 rounded-md">
+      <div className="bg-black/25 relative p-8 rounded-md">
         <p className="text-2xl text-center pb-6">{'Weather in ' + data.location.name}</p>
         <div className="flex justify-between text-center">
           <div>
